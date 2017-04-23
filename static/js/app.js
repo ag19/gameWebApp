4fd95b371de750b9f031637c7b9b3cf1a8264940
jayant yadav <jayantyadav202@gmail.com>
2017-04-23 21:27:29 +0530

    implemented carousal

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,11 @@
-var app = angular.module('gameApp', ['ui.router', 'angularCSS']);
+var app = angular.module('gameApp', ['ui.router', 'ui.bootstrap', 'angularCSS']);
 
-app.directive('header', function() {
-return {
-    templateUrl: 'static/templates/layout/header.html',
-    css: {
-     href: 'static/css/header.css',
-     preload: true
-   }
-}
+app.directive('header', function () {
+    return {
+        templateUrl: 'static/templates/layout/header.html',
+        css: {
+            href: 'static/css/header.css',
+            preload: true
+        }
+    }
 });
--- static/js/controller/mainCtrl.js
+++ static/js/controller/mainCtrl.js
@@ -1,5 +1,28 @@
-app.controller('mainCtrl', ['$scope', function($scope) {
+app.controller('mainCtrl', ['$scope', function ($scope) {
     $scope.message = "Home page text";
+
+    $scope.singleModel = 1;
+    $scope.myInterval = 5000;
+    $scope.active = 0;
+    //    var slides = $scope.slides = [];
+
+    $scope.slides = [
+        {
+            image: 'https://images7.alphacoders.com/386/386016.png'
+    },
+        {
+            image: 'https://s.candybanana.com/images/0b17/forza_horizon_2_55.jpg'
+    },
+        {
+            image: 'https://media.playstation.com/is/image/SCEA/prey-listing-thumb-01-ps4-us-14jun16?$Icon$'
+    },
+        {
+            image: 'https://media.playstation.com/is/image/SCEA/fifa-17-standard-edition-two-column-01-ps4-us-03jun16?$MediaCarousel_Original$'
+    },
+        {
+            image:'http://www.dtxgames.com/wp-content/uploads/2016/11/2389681-assassins-creed-4-black-flag-hd-wallpaper1.jpg'  
+        }
+  ];
 
 
 }]);
