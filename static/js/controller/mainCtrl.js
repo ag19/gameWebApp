220fdc1cc04bf3aca3dc8768e534d0cf4b1ac4ef
Manish Sharma <manish.convegenius@gmail.com>
2017-04-23 10:40:08 +0530

    header work

--- static/js/controller/mainCtrl.js
+++ static/js/controller/mainCtrl.js
@@ -1,4 +1,4 @@
 app.controller('mainCtrl', ['$scope', function($scope) {
     $scope.message = "Home page text";
-    $scope.header = "/gameWebApp/static/templates/layout/header.html";
+    $scope.header = "static/templates/layout/header.html";
 }]);
