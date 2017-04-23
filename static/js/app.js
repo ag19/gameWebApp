f820e36580903bd7b3467eadbf5d79e7929970d3
jayant yadav <jayantyadav202@gmail.com>
2017-04-23 16:35:26 +0530

    header css added

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,10 @@ var app = angular.module('gameApp', ['ui.router', 'angularCSS']);
 
 app.directive('header', function() {
 return {
-    templateUrl: './static/templates/layout/header.html'
+    templateUrl: 'static/templates/layout/header.html',
+    css: {
+     href: 'static/css/header.css',
+     preload: true
+   }
 }
 });
